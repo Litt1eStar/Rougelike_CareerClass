dc81af7416dca9f05ccdf5c443b787d55b0e8493
Woody <devkritt@gmail.com>
2025-01-03 13:55:31 +0700

    Apply Character Animation based on User Input and Handle with Flip of Sprite based on User Input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,14 +6,46 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 10f;
 
+    Animator anim;
+    SpriteRenderer sr;
     float xInput;
     float yInput;
+
+    bool isMoving = false;  
+    bool isFlip = false;
+    private void Start()
+    {
+        sr = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();  
+
+        if(sr == null)
+        {
+            Debug.LogWarning("SpriteRenderer Component is Missing");
+        }
+
+        if (anim == null)
+        {
+            Debug.LogWarning("Animator Component is Missing");
+        }
+    }
     private void Update()
     {
         GetMovementInput();
         MoveCharacter();
+        FlipCharacter();
+        HandleCharacterAnimation();
     }
-
+    private void HandleCharacterAnimation()
+    {
+        isMoving = xInput != 0 || yInput != 0;
+        anim.SetBool("isMoving", isMoving);
+    }
+    private void FlipCharacter()
+    {
+        if(xInput < 0 && !isFlip) isFlip = true;
+        else if(xInput > 0 && isFlip) isFlip = false;
+        sr.flipX = isFlip;
+    }
     private void GetMovementInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
@@ -22,6 +54,6 @@ public class Player : MonoBehaviour
 
     private void MoveCharacter()
     {
-        transform.position += new Vector3(xInput * movementSpeed * Time.deltaTime, yInput * movementSpeed * Time.deltaTime, 0);
+        transform.position += new Vector3(xInput, yInput).normalized * movementSpeed * Time.deltaTime;
     }
 }
