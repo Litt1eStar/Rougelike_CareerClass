9ea3b0bb202ef4ef69018d92b7c447864337e1bb
Woody <devkritt@gmail.com>
2025-01-08 13:31:10 +0700

    Create Magnet for Collect Exp and Create Animation for Exp move to Player

--- Assets/Scripts/Exp.cs
+++ Assets/Scripts/Exp.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,28 @@ using UnityEngine;
 public class Exp : MonoBehaviour
 {
     [SerializeField] private float expValue = 10f;
+    [SerializeField] private float moveSpeed = 20f;
+
+    private bool isMoving = false;
+    public void MoveTo(Vector3 destinationPosition)
+    {
+        if (!isMoving)
+        {
+            StartCoroutine(AnimateExp(destinationPosition));
+        }
+    }
+
+    private IEnumerator AnimateExp(Vector3 destinationPosition)
+    {
+        isMoving = true;
+        while (Vector3.Distance(this.transform.position, destinationPosition) > 0.1f)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, destinationPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        this.transform.position = destinationPosition;
+        isMoving = false;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
---
+++ Assets/Scripts/Player/PlayerMagnet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMagnet : MonoBehaviour
+{
+    [SerializeField] private float magnetRange = 5f;
+
+    private void Update()
+    {
+        Magnet();
+    }
+    private void Magnet()
+    {
+        Collider2D[] expOnGround = Physics2D.OverlapCircleAll(this.transform.position, magnetRange, LayerMask.GetMask("EXP"));
+
+        if (expOnGround.Length > 0) 
+        {
+            foreach (var exp in expOnGround)
+            {
+                exp.GetComponent<Exp>().MoveTo(this.transform.position);
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        
+    }
+}
